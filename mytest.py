2f48458086ee53e301265b73ac8808453ede2880
ManuelMBaumann <manuelmbaumann@gmail.com>
2019-09-19 18:15:41 +0200

    restruct

--- mytest.py
+++ mytest.py
@@ -1,6 +1,4 @@
+import numpy as np
+
 def calc_square(x):
   return x**2
-  
-  
-a = 3
-assert( calc_square(a) == 9 )
---
+++ test1.py
@@ -0,0 +1,4 @@
+from mytest import calc_square
+a  = 3
+a2 = calc_square(a)
+assert(a2==9)

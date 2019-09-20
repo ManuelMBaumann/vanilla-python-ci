bdb22d85d392bf2fb3b4912c93d7188a38c85616
ManuelMBaumann <manuelmbaumann@gmail.com>
2019-09-20 15:55:47 +0200

    upd documentation

--- test-ci/main.py
+++ test-ci/main.py
@@ -1,5 +1,17 @@
 import numpy as np
 
 def calc_square(x):
-'''A simple function.'''
-  return np.power(x, 2)
+    '''A simple function.
+    
+    Parameters
+    ----------
+    x : NumPy array
+        Array input.
+    
+    Returns
+    -------
+    NumPy array
+        The element-wise square.
+    
+    '''
+    return np.power(x, 2)

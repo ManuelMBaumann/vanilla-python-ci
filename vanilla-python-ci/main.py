04c0c8d3a664d671b3a3b882d6a41e11ac8ec23d
ManuelMBaumann <manuelmbaumann@gmail.com>
2019-09-24 10:28:06 +0200

    PEP8 for main.py

--- vanilla-python-ci/main.py
+++ vanilla-python-ci/main.py
@@ -1,20 +1,24 @@
+#!/usr/bin/env python
+"""Two function out of which one is covered by unit tests"""
+
 import numpy as np
 
-def calc_square(x):
+def calc_square(arg1):
     r'''A simple function.
-    
+
     Parameters
     ----------
     x : NumPy array
         Array input.
-    
+
     Returns
     -------
     NumPy array
         The element-wise square.
-    
-    '''
-    return np.power(x, 2)
 
-def calc_square_root(x):
-    return np.sqrt(x)
+    '''
+    return np.power(arg1, 2)
+
+def calc_square_root(arg1):
+    r'''A second function that is not tested.'''
+    return np.sqrt(arg1)
